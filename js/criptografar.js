859223846d2b1849e446f77f22f985811ef596f9
Tati <tatiani60@gmail.com>
2022-01-26 19:14:09 -0300

    ajustes

---
+++ js/criptografar.js
@@ -0,0 +1,19 @@
+var botao = document.querySelector("#criptografar");
+
+function criptografar () {
+    var input = document.querySelector("#input");
+    if (input.value = a) {
+        input.value = ("ai")
+    } if (input.value = e) {
+        input.value= ("enter")
+        
+    }
+}
+
+
+botao.addEventListener("click", function() {
+   alert("cliquei");
+})    
+
+
+
