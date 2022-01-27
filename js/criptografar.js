5599ca8e81573ffb425209f0976097b768a5b169
Tati <tatiani60@gmail.com>
2022-01-26 22:00:50 -0300

    criando o comando de criptografar

--- js/criptografar.js
+++ js/criptografar.js
@@ -1,19 +1,41 @@
-var botao = document.querySelector("#criptografar");
+var botaoCriptografar = document.querySelector("#criptografar");
 
-function criptografar () {
-    var input = document.querySelector("#input");
-    if (input.value = a) {
-        input.value = ("ai")
-    } if (input.value = e) {
-        input.value= ("enter")
+var criptografia = document.querySelector(".input");
+
+var descriptografar = document.querySelector("#input");
+
+botaoCriptografar.addEventListener("click", function() {
+    
+    
+    var mensagem = criptografia.value;
+    descriptografar.value = mensagem.toLowerCase();
+    
+    function criptografar(){
+        var a = mensagem.replace(/a/g, "ai");
+        
+        var e = mensagem.replace(/e/g, "enter");
+        
+        var i = mensagem.replace(/i/g, "imes");
+        
+        var o = mensagem.replace(/o/g, "ober");
+        
+        var u = mensagem.replace(/u/g, "ufat");
+
+        //return(a)
+
+        // talvez um variavel só?
         
     }
-}
+   
+    console.log(criptografar.value);
+
+    })    
 
 
-botao.addEventListener("click", function() {
-   alert("cliquei");
-})    
+
+
+
+
 
 
 
